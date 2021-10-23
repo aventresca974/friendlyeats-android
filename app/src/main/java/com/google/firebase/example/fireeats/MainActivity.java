6c758ee8b66248166970c9abd438605ebbda252e
aventresca974 <90363918+aventresca974@users.noreply.github.com>
2021-10-23 15:13:07 -0400

    initial commit

--- app/src/main/java/com/google/firebase/example/fireeats/MainActivity.java
+++ app/src/main/java/com/google/firebase/example/fireeats/MainActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package com.google.firebase.example.fireeats;
+ package com.google.firebase.example.fireeats;  
 
 import android.content.Intent;
 import android.os.Bundle;
